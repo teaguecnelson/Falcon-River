1fcb0e444d11236f7de156c9ad946090a050f9a5
Teague Nelson <teague@owlmountian.co>
2019-07-30 02:49:38 -0500

    styling visual pattern

---
+++ page-templates/standardPage.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * A template to...
+ *
+ * Template Name: Standard Page
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0-or-later
+ * @link    https://www.studiopress.com/
+ */
+
+// Move Entry Header to a Full-width Hook 
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+add_action( 'genesis_after_header', 'genesis_entry_header_markup_open', 5 );
+add_action( 'genesis_after_header', 'genesis_do_post_title' );
+add_action( 'genesis_after_header', 'genesis_post_info' );
+add_action( 'genesis_after_header', 'genesis_entry_header_markup_close', 15 );
+// Add custom opening div for page title
+add_action( 'genesis_after_header', 'sk_do_page_title_before', 7 );
+function sk_do_page_title_before() {
+	echo '<div class="alignfull"><div class="site-inner"><div class="content-sidebar-wrap"><div class="content"><div class="wp-block-media-text alignwide has-media-on-the-right"><div class="wp-block-media-text__content my-entry-title">';
+}
+
+// Add custom closing div for page title
+add_action( 'genesis_after_header', 'sk_do_page_title_after' );
+function sk_do_page_title_after() {
+	echo 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.
+
+	</div></div></div></div></div></div>';
+}
+
+// Runs the Genesis loop.
+genesis();
